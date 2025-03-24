14ddc82c5ec9f8872f88bbd7b0d058169cbeab21
Marcel Graber <marcel@clever.design>
2025-03-24 11:32:22 +0100

    feat: remove exmple apps

--- examples/zephyr/hello_world/src/app.c
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2025 Clever Design (Switzerland) GmbH
-// SPDX-License-Identifier: Apache-2.0
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/net/socket.h>
-int main(void) {}
