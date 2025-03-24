338deee52da62b31e97b78f33c2d29ab1bf3549f
Marcel Graber <marcel@clever.design>
2025-03-24 15:54:33 +0100

    feat: use ssh git module

--- platform/zephyr.cmake
+++ platform/zephyr.cmake
@@ -1,5 +1,8 @@
 cmake_minimum_required(VERSION 3.20)
 
+zephyr_library()
+zephyr_library_sources(${TINYCOMMON_DIR}/src/empty_file.c)
+
 add_library(tinylibs_interface INTERFACE)
 tiny_library_named(tinylibs)
 tiny_include_directories(${TINYPLATFORM_DIR}/include)
